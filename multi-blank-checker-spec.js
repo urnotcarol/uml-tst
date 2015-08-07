5d3beb383b8606dbf695f542fda1fca0f509e4fa
Chrissie <urnotchrissie@outlook.com>
2015-08-07 11:58:08 +0800

    commit a 'it' of MultiBlankChecker

---
+++ multi-blank-checker-spec.js
@@ -0,0 +1,21 @@
+describe("MultiBlankChecker", function(){
+  describe("checkAnswer", function() {
+    var theMultiBlankChecker;
+    beforeEach(function() {
+      var key = [{
+        name: "Q1-2",
+        value: ["封装性", "继承性", "多态性"],
+        point: 3}];
+        theMultiBlankChecker = new MultiBlankChecker(key);
+    });
+    it("should return correct score", function(){
+      var answer = {
+        type: "text",
+        name: "Q1-2",
+        value: ["封装性", "封装性", "多态性"]
+      };
+      var result = theMultiBlankChecker.checkAnswer(answer);
+      expect(result).toBe(2);
+    });
+  });
+});
